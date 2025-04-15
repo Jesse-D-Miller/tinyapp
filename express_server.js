4ed09cd2a375472a9d487ba64e122b24751996c6
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-14 21:01:30 -0700

    defined and provided logic for random string generator, this will be used to create short urls. currently its in the wrong spot probably

--- express_server.js
+++ express_server.js
@@ -4,6 +4,25 @@ const PORT = 8080; //default port
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() { //generate string of 6 aplhanumeric chars
+  const length = 6;
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+ 
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    result += charset[randomIndex];
+  }
+ 
+  return result;
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -27,8 +46,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  // const shortURL =req.params.id;
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
