82a6386f7fafd19693d727ae11727e00c550ac53
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-18 13:02:10 -0700

    implemented test cases for getUserByEmail function

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail(testUsers, "user@example.com")
+    const expectedUserID = "userRandomID";
+    assert.strictEqual(user.id, expectedUserID)
+  });
+
+  it('should return null if input email is not in database', function() {
+    const user = getUserByEmail(testUsers, "superCoolUser@example.com")
+    const expectedUserID = null;
+    assert.strictEqual(user, expectedUserID)
+  });
+
+});
